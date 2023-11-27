29e8f8d663ab142efe0e27b50a70b0749df06904
FahdAzouz <fahdstudies00@gmail.com>
2023-11-27 16:54:33 +0100

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -3,19 +3,16 @@
 /**
  * binary_tree_inorder - Prints the nodes of a binary tree in inorder.
  * @tree: Pointer to the root node of the tree.
- * @func: prints value
+ * @func: Prints value.
+ *
  * Description: Prints the nodes of a binary tree in inorder.
  */
-
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
-	if (!tree->right && !tree->left)
-	{
-		return;
-	}
-	binary_tree_preorder(tree->left, func);
-    func(tree->n);
-	binary_tree_preorder(tree->right, func);
+        if (!tree || !func)
+                return;
+
+        binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        binary_tree_inorder(tree->right, func);
 }
