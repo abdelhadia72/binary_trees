9a2cd41af47363e9fbd5a655ac81cce0718030fe
FahdAzouz <fahdstudies00@gmail.com>
2023-11-27 17:45:23 +0100

    header

--- binary_trees.h
+++ binary_trees.h
@@ -30,4 +30,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
+size_t binary_tree_height(const binary_tree_t *tree);
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* _BINARY_TREES_H_ */
