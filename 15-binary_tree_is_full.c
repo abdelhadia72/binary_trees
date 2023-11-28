897192f69d9438e3cbdc9114784c737741c18511
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 08:45:48 +0100

    Is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    if (!tree->left && !tree->right)
+        return (1);
+
+    if (tree->left && tree->right)
+        return ((tree->left) && binary_tree_is_full(tree->right));
+
+    return (0);
+}
