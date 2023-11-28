bbff5988dff6a25fa0e40aaaa1fcef1eaf2d903e
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 11:07:49 +0100

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height = 0;
-    size_t right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    if (tree->left)
-        left_height = 1 + binary_tree_height(tree->left);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 
-    if (tree->right)
-        right_height = 1 + binary_tree_height(tree->right);
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
 
-    return (left_height > right_height) ? left_height : right_height;
+	return (left_height > right_height) ? left_height : right_height;
 }
