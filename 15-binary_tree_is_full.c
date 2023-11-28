edb0f32f2a077c7a6f357461ca0c911ca6ac4685
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 12:07:52 +0100

    fixing 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
         return (1);
 
     if (tree->left && tree->right)
-        return ((tree->left) && binary_tree_is_full(tree->right));
+        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
     return (0);
 }
