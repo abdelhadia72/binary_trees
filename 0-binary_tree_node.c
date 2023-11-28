95edbb72d4b833aa0bbe23be14a35a6e8016b94a
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 10:01:39 +0100

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,25 +10,19 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = NULL;
 
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 
+	/* Initialize new node */
+	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	new_node->parent = parent;
 
-	if (parent)
-	{
-		if (!parent->left)
-			parent->left = new_node;
-		else if (!parent->right)
-			parent->right = new_node;
-		else
-			return (NULL);
-	}
-
+	/* Return pointer to new node */
 	return (new_node);
 }
