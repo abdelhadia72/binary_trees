9b25ae981470fa6a52833d637127f8f5b61bfd73
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 11:42:59 +0100

    Refactor binary_tree_height function to improve readability and efficiency

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,18 +10,28 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height = 0;
-    size_t right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    left_height = 1 + binary_tree_height(tree->left);
-    right_height = 1 + binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 
-    return (left_height > right_height) ? left_height : right_height;
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ? left_height : right_height);
 }
 
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Description: Measures the balance factor of a binary tree.
+ * Return: Balance factor of the tree.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
     if (!tree)
