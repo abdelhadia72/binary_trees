f7e442771a72864936ba2bf5e84e0dec35fb577e
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 10:35:21 +0100

    err

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,16 +9,18 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (!parent)
 		return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
+	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
-
+	
 	new_node->n = value;
 	new_node->parent = parent;
+	new_node->left = NULL;
 	new_node->right = NULL;
 
 	if (parent->left)
@@ -26,11 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	else
-		new_node->left = NULL;
-	
 	parent->left = new_node;
 
 	return (new_node);
-	
 }
