fa1464cfa40b4f7f6134d3a6361bac64ea6ff1a1
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 10:59:42 +0100

    inorder

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -9,10 +9,10 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (!tree || !func)
-                return;
+	if (!tree || !func)
+		return;
 
-        binary_tree_inorder(tree->left, func);
-        func(tree->n);
-        binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
