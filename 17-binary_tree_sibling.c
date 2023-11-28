dd90f280c600a11ce8cbb715a575a23f703bf693
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 17:04:56 +0100

    fixing 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,14 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_sibling - Finds the sibling of a binary tree node.
+ * @node: Pointer to the node to find the sibling.
+ *
+ * Return: Pointer to the sibling node, or NULL if no sibling exists.
+ */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return NULL;
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
-    else if (node->parent->right == node)
-        return (node->parent->left);
-    else
-        return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
+	else
+		return (NULL);
 }
