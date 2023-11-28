f2de5fc9e9a7eb7e07a89ae0c68938423d9eb1ef
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 10:17:45 +0100

    Fixing the MG

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,19 +10,25 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	/* Allocate memory for new node */
-	new_node = malloc(sizeof(binary_tree_t));
 	if (!new_node)
 		return (NULL);
 
-	/* Initialize new node */
-	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
+	new_node->parent = parent;
 
-	/* Return pointer to new node */
+	if (parent)
+	{
+		if (!parent->left)
+			parent->left = new_node;
+		else if (!parent->right)
+			parent->right = new_node;
+		else
+			return (NULL);
+	}
+
 	return (new_node);
 }
