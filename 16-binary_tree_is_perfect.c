76d134df5ce8f444952f88c1c79469e2468773c8
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 09:14:56 +0100

    check if it's perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Description: Measures the height of a binary tree.
+ * Return: Height of the tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height = 0;
+    size_t right_height = 0;
+
+    if (!tree)
+        return (0);
+
+    left_height = 1 + binary_tree_height(tree->left);
+    right_height = 1 + binary_tree_height(tree->right);
+
+    return (left_height > right_height) ? left_height : right_height;
+}
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    size_t left_height, right_height;
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height == right_height) ? 1 : 0;
+}
