ccbaf6ce7a4ebfbadfdd2221b8597d52771e8137
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 11:06:34 +0100

    betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -10,11 +10,11 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (!tree || !func)
-                return;
+	if (!tree || !func)
+		return;
 
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-        func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
 
