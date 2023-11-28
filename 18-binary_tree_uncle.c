5d489c4d54da482afaf8d8afd459277b1f5af9be
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 17:21:02 +0100

    forget my dad

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node)
 		return (NULL);
 
-	if (node->parent->parent->left == node)
+	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
-	else if (node->parent->parent->right == node)
+	else if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
 	else
 		return (NULL);
