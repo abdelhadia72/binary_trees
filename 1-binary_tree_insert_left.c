4f7edfd3582e23a5155da86c985103c016657af3
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 09:41:09 +0100

    Fixing Task 1 v.02🪲

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,9 +9,6 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	if (!value)
-		return (NULL);
-
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (!new_node)
