56641ad46f83705680659c5ba01ea8a64f05c394
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 10:53:50 +0100

    preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,19 +2,20 @@
 
 /**
  * binary_tree_preorder - Prints the nodes of a binary tree in preorder.
-  * @tree: Pointer to the root node of the tree.
-  * @func: prints value
-  * Description: Prints the nodes of a binary tree in preorder.
+ * @tree: Pointer to the root node of the tree.
+ * @func: Pointer to a function to call for each node.
+ *
+ * Description: Prints the nodes of a binary tree in preorder.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	func(tree->n);
 	if (!tree || !func)
 		return;
+
 	if (!tree->right && !tree->left)
-	{
 		return;
-	}
+
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
