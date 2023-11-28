5cff2f0007ae0f07d0e1a4083fc5004b88b7d88b
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 11:27:08 +0100

    betty

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -14,5 +14,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
