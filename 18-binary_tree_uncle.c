97d075646147d88d6eae0284c0656ad05100dad9
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 17:23:05 +0100

    now let's fix it

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,6 +12,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node)
 		return (NULL);
 
+	if (!node->parent->parent->left)
+		return (NULL);
+
+	if (!node->parent->parent->right)
+		return (NULL);
+
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 	else if (node->parent->parent->right == node->parent)
