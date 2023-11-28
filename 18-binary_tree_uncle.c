2662cb500cc5aadc701702d1c4147a17248513f3
abdelhadia72 <thisisavnb@gmail.com>
2023-11-28 17:33:54 +0100

    fixing task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node)
+	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
 	if (node->parent->parent->left && node->parent->parent->right)
