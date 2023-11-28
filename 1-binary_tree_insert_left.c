f2b0d9c57f46902bc81093b6572dab54b06207dc
FahdAzouz <fahdstudies00@gmail.com>
2023-11-28 10:28:53 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,22 +17,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
 	new_node->parent = parent;
+	new_node->right = NULL;
 
-	if (parent)
+	if (parent->left)
 	{
-		if (!parent->left)
-			parent->left = new_node;
-		else
-		{
-			new_node->left = parent->left;
-			parent->left->parent = new_node;
-			parent->left = new_node;
-		}
-		
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
+	else
+		new_node->left = NULL;
+	
+	parent->left = new_node;
 
 	return (new_node);
 }
