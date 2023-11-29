7788e671b4494d5cd3a8bbdd24e8c84ecf79feb9
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 13:00:02 +0100

    rotl v.4 rechange other side **&

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -3,21 +3,21 @@
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	if (!tree || !tree->right)
-		return NULL;
+		return (NULL);
 
 	binary_tree_t *old_root, *new_root, *old_node;
 
 	old_root = tree;
-	new_root = tree->left;
-	old_node = tree->left->right;
+	new_root = tree->right;
+	old_node = tree->right->left;
 
 	new_root->left = old_root;
 	old_root->parent = new_root;
-	old_node->right = old_node;
+	old_root->right = old_node;
 
-	if (old_node->right)
+	if (old_node)
 	{
-		old_node->parent = new_root->left;
+		old_node->parent = old_root;
 	}
 
 	return (new_root);
