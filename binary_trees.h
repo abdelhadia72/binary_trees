83d6fcd399ded83cd8b08a388d7f8de9206e6bdf
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 16:21:10 +0100

    adding bst_t ⭐

--- binary_trees.h
+++ binary_trees.h
@@ -15,7 +15,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+} binary_tree_t, bst_t;
 
 /* library */
 #include <stdio.h>
