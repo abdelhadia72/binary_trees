959f0cafb293bc563833de8fc5833ba7751941ea
FahdAzouz <fahdstudies00@gmail.com>
2023-11-29 14:39:47 +0100

    commit

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+    f:
+    24 on
+    25 off
+    26 off
+    27 off
+
+}
