981dfe532aa6f56b9b8ac7f19ae8d577cf1c9190
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 14:13:15 +0100

    rotr v2 (betty)

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -9,18 +9,18 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
+	binary_tree_t *new_root;
 
-    if (tree == NULL)
-        return (NULL);
+	if (tree == NULL)
+		return (NULL);
 
-    new_root = tree->left;
-    tree->left = new_root->right;
-    if (new_root->right != NULL)
-        new_root->right->parent = tree;
-    new_root->right = tree;
-    new_root->parent = tree->parent;
-    tree->parent = new_root;
+	new_root = tree->left;
+	tree->left = new_root->right;
+	if (new_root->right != NULL)
+		new_root->right->parent = tree;
+	new_root->right = tree;
+	new_root->parent = tree->parent;
+	tree->parent = new_root;
 
-    return (new_root);
+	return (new_root);
 }
