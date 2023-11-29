92021d585b0e407ed12d4154695f1bc24df5264e
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 20:09:21 +0100

    update nothing adding req 121

--- binary_trees.h
+++ binary_trees.h
@@ -60,4 +60,5 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 int binary_tree_is_avl_height(const binary_tree_t *tree);
+avl_t *avl_insert(avl_t **tree, int value);
 #endif /* _BINARY_TREES_H_ */
