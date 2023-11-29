b0f5b7940cefc5e95219d6c4c663a177ab7a4c70
FahdAzouz <fahdstudies00@gmail.com>
2023-11-29 17:27:24 +0100

    array to bst

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -7,15 +7,48 @@
  *
  * Return: Pointer to the root node of the created BST, or NULL on failure.
  */
-
 bst_t *array_to_bst(int *array, size_t size)
 {
-    bst_t *root, *temp;
-    root = binary_tree_node(NULL, array[0]);
+	if (array == NULL || size == 0)
+		return NULL;
 
-    while (size)
-    {
-        
-        binary_tree_node(tree,array[size])
-    }
+	bst_t *root = binary_tree_node(NULL, array[0]);
+	if (root == NULL)
+		return NULL;
+
+	for (size_t i = 1; i < size; i++)
+	{
+		if (bst_search(root, array[i]) == NULL)
+		{
+			if (bst_insert(root, array[i]) == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+
+	return root;
+}
+
+/**
+ * bst_search - Searches for a value in a Binary Search Tree.
+ * @tree: A pointer to the root node of the BST to search.
+ * @value: The value to search for.
+ *
+ * Return: If the tree is NULL or the value is not found, NULL.
+ *         Otherwise, a pointer to the node containing the value.
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return NULL;
+
+	if (tree->n == value)
+		return (bst_t *)tree;
+
+	if (tree->n > value)
+		return bst_search(tree->left, value);
+	else
+		return bst_search(tree->right, value);
 }
