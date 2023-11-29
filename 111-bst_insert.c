11455305663b83b9466665bfcac72a497a10b77c
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 18:26:11 +0100

    fixing 111

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -21,14 +21,20 @@ bst_t *bst_insert(bst_t **tree, int value)
 	if (value < (*tree)->n)
 	{
 		if (!(*tree)->left)
-			return (binary_tree_node((*tree), value));
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
 		return (bst_insert(&((*tree)->left), value));
 	}
 	/* go right */
 	else if (value > (*tree)->n)
 	{
 		if (!(*tree)->right)
-			return (binary_tree_node((*tree), value));
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
 		return (bst_insert(&((*tree)->right), value));
 	}
 
