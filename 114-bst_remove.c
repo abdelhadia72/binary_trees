a0b94c000cc38be416d3333e7bb6308c26de1eef
FahdAzouz <fahdstudies00@gmail.com>
2023-11-29 18:20:24 +0100

    remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -5,41 +5,51 @@
  * @root: Pointer to the root node of the BST
  * @value: Value to remove in the tree
  *
- * Return: Pointer to the new root node of the tree after removing the desired
- * value
+ * Return: Pointer to the new root node of the tree after removing the desired value
  */
-
 bst_t *bst_remove(bst_t *root, int value)
 {
-    if (!root)
-        return (0);
-    
-    if (root->n == value)
-    {
-        if (root->right || root->left)
-        {
-            root->right->parent = root->parent;
-            root->left->parent = root->right;
-        }
-        if (root->parent)
-        {
-            bst_t *parnt = root->parent;
-        }
-        free(root);
-        return (parnt);
-    }
-    /* Check left*/
-    if (root->n > value)
-    {
-        if (!root->left)
-            return 0;
-        bst_remove(root->left, value);
-    }
-    /*Check right*/
-    if (root->n < value)
-    {
-        if (!root->right)
-            return 0;
-        bst_remove(root->right, value);
-    }
+	if (!root)
+		return NULL;
+
+	if (root->n == value)
+	{
+		bst_t *new_root = NULL;
+
+		if (root->right && root->left)
+		{
+			bst_t *successor = root->right;
+
+			while (successor->left)
+				successor = successor->left;
+
+			root->n = successor->n;
+			root->right = bst_remove(root->right, successor->n);
+		}
+		else if (root->right)
+		{
+			new_root = root->right;
+			new_root->parent = root->parent;
+			free(root);
+		}
+		else if (root->left)
+		{
+			new_root = root->left;
+			new_root->parent = root->parent;
+			free(root);
+		}
+		else
+		{
+			free(root);
+		}
+
+		return new_root;
+	}
+
+	if (root->n > value)
+		root->left = bst_remove(root->left, value);
+	else
+		root->right = bst_remove(root->right, value);
+
+	return root;
 }
