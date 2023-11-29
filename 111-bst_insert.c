eb15e3957208373e713e8d16c398ce5b4fc127aa
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 17:22:24 +0100

    fix betty insert bst 25

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,33 +1,41 @@
 #include "binary_trees.h"
 
+/**
+ * bst_insert - Inserts a value in a Binary Search Tree
+ * @tree: Double pointer to the root node of the BST
+ * @value: Value to store in the node to be inserted
+ *
+ * Return: Pointer to the created node, or NULL on failure
+ */
+
 bst_t *bst_insert(bst_t **tree, int value)
 {
 
-    if (!tree)
-        return (NULL);
+	if (!tree)
+		return (NULL);
 
-    if (!(*tree))
-    {
-        *tree = binary_tree_node(NULL, value);
-        return (*tree);
-    }
+	if (!(*tree))
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
 
-    /* go left */
-    if ((*tree)->n > value)
-    {
-        if (!(*tree)->left)
-            return (binary_tree_node((*tree), value));
-        else
-            return (bst_insert(&((*tree)->left), value));
-    }
-    /* go right */
-    else if ((*tree)->n < value)
-    {
-        if (!(*tree)->right)
-            return (binary_tree_node((*tree), value));
-        else
-            return (bst_insert(&((*tree)->right), value));
-    }
+	/* go left */
+	if ((*tree)->n > value)
+	{
+		if (!(*tree)->left)
+			return (binary_tree_node((*tree), value));
+		else
+			return (bst_insert(&((*tree)->left), value));
+	}
+	/* go right */
+	else if ((*tree)->n < value)
+	{
+		if (!(*tree)->right)
+			return (binary_tree_node((*tree), value));
+		else
+			return (bst_insert(&((*tree)->right), value));
+	}
 
-    return (NULL);
+	return (NULL);
 }
