379cc9d0b8af5acce89d752783be21fec4addd54
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 17:09:51 +0100

    insert in bst v1

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -2,4 +2,32 @@
 
 bst_t *bst_insert(bst_t **tree, int value)
 {
+
+    if (!tree)
+        return (NULL);
+
+    if (!(*tree))
+    {
+        *tree = binary_tree_node(NULL, value);
+        return (*tree);
+    }
+
+    /* go left */
+    if ((*tree)->n > value)
+    {
+        if (!(*tree)->left)
+            return ((*tree)->left = binary_tree_node((*tree), value));
+        else
+            return (bst_insert(&((*tree)->left), value));
+    }
+    /* go right */
+    else if ((*tree)->n < value)
+    {
+        if (!(*tree)->right)
+            return ((*tree)->right = binary_tree_node((*tree), value));
+        else
+            return (bst_insert(&((*tree)->right), value));
+    }
+
+    return (NULL);
 }
