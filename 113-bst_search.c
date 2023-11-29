991fa65010015f77b0e45b6d9d472437b8754431
abdelhadia72 <thisisavnb@gmail.com>
2023-11-29 19:17:11 +0100

    Seach in bst

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -2,4 +2,14 @@
 
 bst_t *bst_search(const bst_t *tree, int value)
 {
+    if (!tree)
+        return (NULL);
+
+    if (tree->n == value)
+        return ((bst_t *)tree);
+
+    if (tree->n >= value)
+        return (bst_search(tree->left, value));
+
+    return (bst_search(tree->right, value));
 }
