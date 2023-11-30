79ea1303e0b2461f5e285bb4e03f5e20d8197155
FahdAzouz <fahdstudies00@gmail.com>
2023-11-30 15:20:12 +0100

    36

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -8,25 +8,17 @@
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-    if (!tree)
-        return 0;
+	if (!tree)
+		return 0;
 
-    if (tree->left)
-        return (binary_tree_is_heap(tree->left));
-    
-    if (tree->right)
-        return (binary_tree_is_heap(tree->right));
+	if (tree->left && tree->n < tree->left->n)
+		return 0;
 
-    if (tree->right)
-    {
-        if (tree->n < tree->right->n)
-            return 0;
-    }
+	if (tree->right && tree->n < tree->right->n)
+		return 0;
 
-    if (tree->left)
-    {
-        if (tree->n < tree->left->n)
-            return 0;
-    }
-    return 1;
+	if (!binary_tree_is_heap(tree->left) || !binary_tree_is_heap(tree->right))
+		return 0;
+
+	return 1;
 }
