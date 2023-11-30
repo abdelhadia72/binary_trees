ddfc36c10e3dfb1808a61d1da4a332ad82d710a4
FahdAzouz <fahdstudies00@gmail.com>
2023-11-30 16:05:14 +0100

    heap insert

---
+++ 131-heap_insert.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - builds a Max Binary Heap tree from an array
+ *
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ * Return: pointer to the root node of the created Binary Heap, or NULL on failure
+ */
+
+
+heap_t *array_to_heap(int *array, size_t size)
+{
+    
+}
--- binary_trees.h
+++ binary_trees.h
@@ -65,4 +65,6 @@ avl_t *array_to_avl(int *array, size_t size);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 void create_tree(avl_t **node, int *array, size_t size, int mode);
 int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *array_to_heap(int *array, size_t size);
+heap_t *heap_insert(heap_t **root, int value);
 #endif /* _BINARY_TREES_H_ */
