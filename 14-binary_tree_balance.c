531c75ccde190e3f3043cb18be6c218a79ee9f02
abdelhadia72 <thisisavnb@gmail.com>
2023-11-30 18:22:53 +0100

    task 14 fixing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-int get_height(const binary_tree_t *tree);
-
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: Pointer to the root node of the tree.
@@ -9,6 +7,7 @@ int get_height(const binary_tree_t *tree);
  * Description: Measures the balance factor of a binary tree.
  * Return: Balance factor of the tree.
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_height = 0;
@@ -18,6 +17,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	{
 		if (tree->left)
 			left_height = 1 + binary_tree_balance(tree->left);
+
 		if (tree->right)
 			right_height = 1 + binary_tree_balance(tree->right);
 	}
@@ -26,22 +26,4 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 	return (left_height - right_height);
-}
-
-/**
- * get_height - Measures the height of a binary tree.
- * @tree: Pointer to the root node of the tree.
- *
- * Return: Height of the tree.
- */
-int get_height(const binary_tree_t *tree)
-{
-	if (tree)
-	{
-		int left_height = 1 + get_height(tree->left);
-		int right_height = 1 + get_height(tree->right);
-
-		return (left_height > right_height ? left_height : right_height);
-	}
-	return (0);
 }
--- 14-main.c
+++ 14-main.c
@@ -21,6 +21,7 @@ int main(void)
     binary_tree_insert_right(root->left, 50);
     binary_tree_insert_left(root->left->left, 10);
     binary_tree_insert_left(root->left->left->left, 8);
+    binary_tree_insert_left(root->left->left->left->left, 3);
     binary_tree_print(root);
 
     balance = binary_tree_balance(root);
@@ -29,5 +30,7 @@ int main(void)
     printf("Balance of %d: %+d\n", root->right->n, balance);
     balance = binary_tree_balance(root->left->left->right);
     printf("Balance of %d: %+d\n", root->left->left->right->n, balance);
+    balance = binary_tree_balance(root->left);
+    printf("Balance of %d: %+d\n", root->left->n, balance);
     return (0);
 }
